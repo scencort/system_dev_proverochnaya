bd760751d6e2b653600905e9b3019dece711b6de
scencort <yaroslavpolyakov28352007@gmail.com>
2025-05-03 13:37:57 +0300

    ...

---
+++ Begin/Begin10.c
@@ -0,0 +1,28 @@
+/*
+Даны два ненулевых числа. Найти сумму, разность, произведение и частное их квадратов. 
+*/
+#include <stdio.h>
+
+int main() {
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+
+    if (a == 0 || b == 0) {
+        return 1;
+    }
+
+    double a2 = a * a;
+    double b2 = b * b;
+
+    double summa = a2 + b2;
+    double raznost = a2 - b2;
+    double proizvedenie = a2 * b2;
+    double chastnoe = a2 / b2;
+
+    printf("%.2lf\n", summa);
+    printf("%.2lf\n", raznost);
+    printf("%.2lf\n", proizvedenie);
+    printf("%.2lf\n", chastnoe);
+
+    return 0;
+}
