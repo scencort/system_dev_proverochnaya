ab5bd30ab3a30048206d16a5c4530a036539a4f1
scencort <yaroslavpolyakov28352007@gmail.com>
2025-05-04 21:05:13 +0300

    ...

---
+++ Text/Text10.c
@@ -0,0 +1,42 @@
+/* +Text10
+Дано целое число K и текстовый файл. Вставить пустую строку после строки файла с номером K. Если строки с таким номером нет, то оставить файл без изменений.
+
+ФИО - Поляков Ярослав Алексеевич
+04.05.2025
+Время выполнения: около 10-11 минут
+*/
+
+#include <stdio.h>
+
+int main() {
+	int k;
+	scanf("%d", &k);
+	
+	FILE *input = fopen("in.txt", "r");
+	if (!input) {
+		return 1;
+	}
+	
+	FILE *output = fopen("out.txt", "w");
+	if (!output) {
+		fclose(input);
+		return 1;
+	}
+	
+	char arr[100];
+	int curr = 1;
+	
+	while (fgets(arr, 100, input)) {
+		fputs(arr, output);
+		if (curr == k) {
+			fputc('\n', output);
+		}
+		curr++;
+	}
+	
+	fclose(input);
+	fclose(output);
+	
+	return 0;
+}
+
